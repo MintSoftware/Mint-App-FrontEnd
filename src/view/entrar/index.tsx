cefb173af7bd2b9bca94e2bebda18e0babbab4f7
GuilhermePeritto <perittoguilherme@gmail.com>
2024-06-03 19:44:56 -0300

    .

--- src/view/entrar/index.tsx
+++ src/view/entrar/index.tsx
@@ -52,50 +52,52 @@ export default function Entrar() {
 
 
     return (
-        <form onSubmit={logar}>
-            <Card className="mx-auto max-w-sm">
-                <CardHeader>
-                    <CardTitle className="text-2xl">Entrar</CardTitle>
-                    <CardDescription>
-                        Insira seu e-mail abaixo para fazer login na sua conta
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <div className="grid gap-4">
-                        <div className="grid gap-2">
-                            <Label htmlFor="email">E-mail</Label>
-                            <Input
-                                id="email"
-                                type="email"
-                                placeholder="jose.silva@mintecommerce.com.br"
-                                required
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div className="grid gap-2">
-                            <div className="flex items-center">
-                                <Label htmlFor="password">Senha</Label>
-                                <Link to="/recuperarsenha" className="ml-auto inline-block text-sm underline">
-                                    Esqueceu sua senha?
-                                </Link>
+        <div className="w-sreen h-screen bg-blue-700 z-50">
+            <form onSubmit={logar}>
+                <Card className="mx-auto max-w-sm w-full h-full">
+                    <CardHeader>
+                        <CardTitle className="text-2xl">Entrar</CardTitle>
+                        <CardDescription>
+                            Insira seu e-mail abaixo para fazer login na sua conta
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="grid gap-4">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">E-mail</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="jose.silva@mintecommerce.com.br"
+                                    required
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
                             </div>
-                            <Input id="password" type="password" placeholder="senha" required onChange={(e) => setSenha(e.target.value)} />
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Senha</Label>
+                                    <Link to="/recuperarsenha" className="ml-auto inline-block text-sm underline">
+                                        Esqueceu sua senha?
+                                    </Link>
+                                </div>
+                                <Input id="password" type="password" placeholder="senha" required onChange={(e) => setSenha(e.target.value)} />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Entrar
+                            </Button>
+                            <Button variant="outline" className="w-full">
+                                Entrar com o Google
+                            </Button>
                         </div>
-                        <Button type="submit" className="w-full">
-                            Entrar
-                        </Button>
-                        <Button variant="outline" className="w-full">
-                            Entrar com o Google
-                        </Button>
-                    </div>
-                    <div className="mt-4 text-center text-sm">
-                        Não tem uma conta?{" "}
-                        <Link to="/criarconta" className="underline">
-                            Cadastre-se
-                        </Link>
-                    </div>
-                </CardContent>
-            </Card>
-        </form>
+                        <div className="mt-4 text-center text-sm">
+                            Não tem uma conta?{" "}
+                            <Link to="/criarconta" className="underline">
+                                Cadastre-se
+                            </Link>
+                        </div>
+                    </CardContent>
+                </Card>
+            </form>
+        </div>
     )
 }
--- src/view/layout/menusuperior/index.tsx
+++ src/view/layout/menusuperior/index.tsx
@@ -1,15 +1,21 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 import { ShoppingCartIcon } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function MenuSuperior() {
     return (
-        <div className="flex flex-row justify-end gap-5 m-3 items-center">
-            <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+        <div className="sticky top-0 z-30 flex h-14 w-full justify-end items-center gap-4 border-b bg-background px-4 pt-2 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+            <Link to='/entrar'>
+                <Button variant={"link"} className="text-white" >Entrar</Button>
+            </Link>
             <div>
-                <ShoppingCartIcon />
+                <div className="fixed left-[97.5%] bg-primary w-5 h-5 border rounded-full flex items-center justify-center">
+                    <Label className="text-black">1</Label>
+                </div>
+                <Button variant={"ghost"} className="ficxe z-30">
+                    <ShoppingCartIcon />
+                </Button>
             </div>
         </div>
     );

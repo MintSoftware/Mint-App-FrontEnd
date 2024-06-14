1ff9812012b9a3061e46d0fc68ec1f37a3c47024
gustavo-gomess <gusgegomes@hotmail.com>
2024-06-13 21:19:26 -0300

    Remove selection column

--- src/view/produto/colunas.tsx
+++ src/view/produto/colunas.tsx
@@ -18,28 +18,7 @@ const ativar = (produto: Produto) => async () => {
 }
 
 export const colunas = (): ColumnDef<Produto>[] => [
-    {
-        id: "select",
-        header: ({ table }) => (
-            <Checkbox
-                checked={
-                    table.getIsAllPageRowsSelected() ||
-                    (table.getIsSomePageRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                aria-label="Select all"
-            />
-        ),
-        cell: ({ row }) => (
-            <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-    }, {
+   {
         accessorKey: 'nome',
         header: ({ column }) => (
             <Cabecalho column={column} title="Nome" />

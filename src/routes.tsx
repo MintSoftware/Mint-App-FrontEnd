3c6da1f8189469a184aa5ae598e7d326c8b18bb0
GuilhermePeritto <perittoguilherme@gmail.com>
2024-06-25 20:37:27 -0300

    ajustes de buld

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,18 +8,12 @@ import { Layout } from "./view/layout";
 import LoadingScreen from "./view/loading/Loading";
 import PaginaNaoEncontrada from "./view/paginanaoencontrada";
 import Pedido from "./view/pedido";
+import EditarPedido from "./view/pedido/editar";
 import FinalizacaoPedido from "./view/pedido/finalizacao";
 import Perfil from "./view/perfil";
 import Produto from "./view/produto";
 import RecuperarSenha from "./view/recuperarsenha";
-<<<<<<< HEAD
-import Pedido from "./view/pedido";
-import DashBoard from "./view/dashboard";
-import PainelProduto from "./view/dashboard/painel";
-import EditarProduto from "./view/produto/editar";
-=======
-import EditarPedido from "./view/pedido/editar";
->>>>>>> 1d5d32dc7cc7bff0dce7f262a4f1f6a386c2892d
+
 
 function MainRoutes() {
     const [isLoading, setIsLoading] = useState(true);
@@ -55,13 +49,9 @@ function MainRoutes() {
                 <Route path="/entrar" element={<Entrar />} />
                 <Route path="/criarconta" element={<CriarConta />} />
                 <Route path="/recuperarsenha" element={<RecuperarSenha />} />
-<<<<<<< HEAD
-                <Route path="/produto/:id/editar" element={<EditarProduto />} /> 
-=======
                 <Route path="/finalizarpedido" element={<FinalizacaoPedido />} />
                 <Route path="/pedido/:id" element={<EditarPedido />} /> 
                 <Route path="/perfil" element={<Perfil />} />
->>>>>>> 1d5d32dc7cc7bff0dce7f262a4f1f6a386c2892d
             </Routes>
         </Layout>
     )

f42f5d87eaf0685be591fba5ede3e4e0f4eeae44
Gustavo Gomes <gusgegomes@hotmail.com>
2024-08-13 21:49:07 -0300

    tela de MenuInicial com correção nas rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,12 @@
 import { useEffect, useState } from "react";
 import { Route, Routes, } from "react-router-dom";
 import CriarConta from "./view/criarconta";
-import DashBoard from "./view/dashboard";
-import PainelProduto from "./view/dashboard/painel";
 import Entrar from "./view/entrar";
 import { Layout } from "./view/layout";
 import LoadingScreen from "./view/loading/Loading";
 import PaginaNaoEncontrada from "./view/paginanaoencontrada";
-import Pedido from "./view/pedido";
-import EditarPedido from "./view/pedido/editar";
-import FinalizacaoPedido from "./view/pedido/finalizacao";
 import Perfil from "./view/perfil";
-import Produto from "./view/produto";
-import RecuperarSenha from "./view/recuperarsenha";
+import MenuInicial from "./view/dashboard";
 
 
 function MainRoutes() {
@@ -41,16 +35,11 @@ function MainRoutes() {
     return (
         <Layout>
             <Routes>
-                <Route path="/" element={<DashBoard />} />
-                <Route path="/produtos/:id" element={<PainelProduto />} />
-                <Route path="/produtos" element={<Produto />} />
-                <Route path="/pedidos" element={<Pedido />} />
+                <Route path="/" element={< MenuInicial />} />
+               {/*} <Route path="/produtos/:id" element={<PainelProduto />} />*/}
                 <Route path="*" element={<PaginaNaoEncontrada />} />
                 <Route path="/entrar" element={<Entrar />} />
                 <Route path="/criarconta" element={<CriarConta />} />
-                <Route path="/recuperarsenha" element={<RecuperarSenha />} />
-                <Route path="/finalizarpedido" element={<FinalizacaoPedido />} />
-                <Route path="/pedido/:id" element={<EditarPedido />} /> 
                 <Route path="/perfil" element={<Perfil />} />
             </Routes>
         </Layout>
